f5ad9d20fe5abddf288396dfdf7084a2b222639b
Federico Martinez <famfededeveloper@gmail.com>
2017-06-10 22:23:14 -0300

    Agrega oja y sanger, hay que revisar si estan bien

--- network.py
+++ network.py
@@ -9,27 +9,50 @@ class UnsupervisedLearningNetwork(object):
         for _ in range(n_salida):
             self.pesos_red.append({'pesos': np.random.uniform(-0.1, 0.1, n_entrada)})
 
-    def train(self, dataset, eta=0.05, epochs=100):
+    def train(self, dataset, eta=0.05, epochs=10, algoritmo="hebb"):
+
+        # for X en D:
+        #     Y = X . W
+        #     for j en [1..M]:
+        #         for i en [1..N]:
+        #             X~_i = 0
+        #             for k en [1..Q]
+        #                 X~_i += Y_k . W_ik
+        #             DeltaW_ij = eta . (X_i - X~_i) . Y_j
+        #     W += DeltaW
 
         for _ in range(epochs):
             
             for _, fila in enumerate(dataset):
+                y = list()
 
                 for n_neurona in range(len(self.pesos_red)):
+                    salida_neurona = np.dot(fila, self.pesos_red[n_neurona]['pesos'])
+                    y.append(salida_neurona)
+                    delta_w = list()
+                    
+                    for i, entrada in enumerate(fila):
+                        x = 0
 
-                    salida_neurona = self.neuron_output(n_neurona, fila)
-                    self.pesos_red[n_neurona]['pesos'] = self.weight_update_hebbian(fila, salida_neurona, eta)
-                    print salida_neurona
+                        for k in range(self.calcular_intervalo(algoritmo, n_neurona)):
+                            x += y[k] * self.pesos_red[k]['pesos'][i]
+
+                        delta_w.append(eta * (entrada - x) * salida_neurona)
+
+                    self.pesos_red[n_neurona]['pesos'] = np.sum([self.pesos_red[n_neurona]['pesos'], delta_w], axis=0)
+                    # print salida_neurona
         
         return self
 
-    def neuron_output(self, neurona, entrada_neurona):
-        return np.dot(entrada_neurona, self.pesos_red[neurona]['pesos'])
+    def calcular_intervalo(self, algoritmo, neurona_actual):
+        if algoritmo == "hebb":
+            return 0
+        
+        if algoritmo == "oja1":
+            return 1
 
-    def weight_update_hebbian(self, entrada_neurona, salida_neurona, eta):
-        updated = list()
-
-        for _, entrada in enumerate(entrada_neurona):
-            updated.append(eta * entrada * salida_neurona) 
-
-        return updated
+        if algoritmo == "oja":
+            return len(self.pesos_red)
+        
+        if algoritmo == "sanger":
+            return neurona_actual
